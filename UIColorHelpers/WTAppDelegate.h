5fd5d16a46c956f26f0e2ee6c816a4c1503bc08e
Joel Garrett <joel.garrett@willowtreeapps.com>
2013-05-08 11:06:15 -0400

    Initial Commit

---
+++ UIColorHelpers/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  UIColorHelpers
+//
+//  Created by Joel Garrett on 5/8/13.
+//  Copyright (c) 2013 WillowTree Apps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
